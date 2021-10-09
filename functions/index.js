99f2d843624d502e2f2185ab7981d8491c114db9
Alex <alex.k.korteacounts@gmail.com>
2021-10-09 16:45:05 -0400

    Updated Functions

--- functions/index.js
+++ functions/index.js
@@ -7,7 +7,6 @@ admin.initializeApp();
 
 // Spotify SDKs to acess the Users Spotify
 const SpotifyWebApi = require("spotify-web-api-node");
-const express = require("express");
 
 // exports.scheduledFunction = functions.pubsub
 //   .schedule("* * * * *")
@@ -16,54 +15,48 @@ const express = require("express");
 //     return null;
 //   });
 const scopes = ["playlist-read-private", "playlist-modify-private"];
-
-const spotifyApi = new SpotifyWebApi({
+const credidentials = {
   redirectUri: "http://localhost:5001/xc-playlist/us-central1/callback",
   clientId: "d869ced3e5734fde862839bba66b096a",
   clientSecret: "59555faa8f734aed9d6d48f5d180a211",
-});
+};
 
 exports.spotifyAuth = functions.https.onRequest(async (req, res) => {
+  const spotifyApi = new SpotifyWebApi(credidentials);
   res.redirect(spotifyApi.createAuthorizeURL(scopes));
 });
 
 exports.callback = functions.https.onRequest(async (req, res) => {
+  const spotifyApi = new SpotifyWebApi(credidentials);
+
   const error = req.query.error;
   const code = req.query.code;
-  const state = req.query.state;
 
   if (error) {
-    console.error("Callback Error:", error);
+    functions.logger.error("Callback Error:", error);
     res.send(`Callback Error: ${error}`);
     return;
   }
 
   spotifyApi
     .authorizationCodeGrant(code)
-    .then((data) => {
-      const access_token = data.body["access_token"];
-      const refresh_token = data.body["refresh_token"];
-      const expires_in = data.body["expires_in"];
+    .then(async (data) => {
+      const accessToken = data.body["access_token"];
+      const refreshToken = data.body["refresh_token"];
+      const expiresIn = data.body["expires_in"];
+      const expiresAt = expiresIn - 120 + Math.floor(Date.now() / 1000);
 
-      spotifyApi.setAccessToken(access_token);
-      spotifyApi.setRefreshToken(refresh_token);
-
-      console.log("access_token:", access_token);
-      console.log("refresh_token:", refresh_token);
+      // Push the new message into Firestore using the Firebase Admin SDK.
+      await admin.firestore().collection("spotifyAPIKeys").doc("stelath").set({
+        refresh_token: refreshToken,
+        access_token: accessToken,
+        expires_at: expiresAt,
+      });
 
       functions.logger.log(
-        `Sucessfully retreived access token. Expires in ${expires_in} s.`
+        `Sucessfully authenticated user, access token expires in ${expiresIn} s.`
       );
       res.send("Success! You can now close the window.");
-
-      setInterval(async () => {
-        const data = await spotifyApi.refreshAccessToken();
-        const access_token = data.body["access_token"];
-
-        console.log("The access token has been refreshed!");
-        console.log("access_token:", access_token);
-        spotifyApi.setAccessToken(access_token);
-      }, (expires_in / 2) * 1000);
     })
     .catch((error) => {
       console.error("Error getting Tokens:", error);
@@ -71,7 +64,61 @@ exports.callback = functions.https.onRequest(async (req, res) => {
     });
 });
 
-exports.getSpotifyPlaylist = functions.https.onRequest(async (req, res) => {});
+exports.getSpotifyPlaylist = functions.https.onRequest(async (req, res) => {
+  const spotifyApi = new SpotifyWebApi(credidentials);
+
+  await admin
+    .firestore()
+    .collection("spotifyAPIKeys")
+    .doc("stelath")
+    .get()
+    .then(async (snap) => {
+      if (snap.exists) {
+        const docData = snap.data();
+
+        const refreshToken = docData.refresh_token;
+        var accessToken = docData.access_token;
+
+        spotifyApi.setRefreshToken(refreshToken);
+
+        if (docData.expires_at < Date.now() / 1000) {
+          const data = await spotifyApi.refreshAccessToken();
+
+          accessToken = data.body["access_token"];
+          const expiresIn = data.body["expires_in"];
+          const expiresAt = expiresIn - 120 + Math.floor(Date.now() / 1000);
+
+          // Push the new Access Token into Firestore using the Firebase Admin SDK.
+          await admin
+            .firestore()
+            .collection("spotifyAPIKeys")
+            .doc("stelath")
+            .set({
+              access_token: accessToken,
+              expires_at: expiresAt,
+            });
+
+          functions.logger.log(
+            `Sucessfully refreshed access token. Expires in ${expiresIn} s.`
+          );
+        }
+
+        spotifyApi.setAccessToken(accessToken);
+      } else {
+        functions.logger.error("Unable to get users Spotify API data");
+      }
+    });
+  
+
+  spotifyApi.getPlaylistTracks("4av5f1eu27t2FSeUAGBChH").then(
+    (data) => {
+      console.log("Tracks data", data);
+    },
+    (err) => {
+      functions.logger.error("Something went wrong!", err);
+    }
+  );
+});
 
 // Take the text parameter passed to this HTTP endpoint and insert it into
 // Firestore under the path /messages/:documentId/original
